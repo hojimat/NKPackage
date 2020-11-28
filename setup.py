16f83f7e4f080b81903eb231ab57583a1ecbdc2a
ravshansk <rsk@ravshansk.com>
2020-11-28 18:35:12 +0100

    reran setup again; now fully

--- setup.py
+++ setup.py
@@ -1,3 +1,18 @@
-from setuptools import setup
-#
-setup(name='nkpack')
+import setuptools
+
+setuptools.setup(
+    name="nkpack-ravshansk", # Replace with your own username
+    version="1.0.0",
+    author="Ravshan S.K.",
+    author_email="rsk@ravshansk.com",
+    description="A small example package",
+    url="https://github.com/ravshansk/nkpack",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
+
