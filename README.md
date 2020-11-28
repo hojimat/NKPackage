0016059cbdddb5e5df8137d2217b0ad8e47ec0ce
ravshansk <rsk@ravshansk.com>
2020-11-28 06:19:50 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+## Helfpul utilities for NK-modeling
+
+#### Infor about NK Models
+Kauffman 1989
+
+#### Info about the package
+
+version 1.0
+
+
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='nkpack',
+      version='1.0',
+      description='A utility package for NK modeling',
+      url='https://github.com/ravshansk/nkpack',
+      author='Ravshan S.K.',
+      author_email='rsk@ravshansk.com'
+      )
