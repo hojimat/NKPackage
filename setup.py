d287239975046f44c47da6769a02c2bfbf603df0
ravshansk <rsk@ravshansk.com>
2020-12-19 06:10:12 +0100

    edited setup to add readme as the longdescription

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
 import setuptools
+from os import path
+
+mydir = path.abspath(path.dirname(__file__))
+with open(path.join(mydir, 'README.md'), encoding='utf-8') as f:
+    longdesc = f.read()
 
 setuptools.setup(
     name="nkpack", # Replace with your own username
     version="1.0.1",
     author="Ravshan S.K.",
     author_email="rsk@ravshansk.com",
-    description="A library of utilities for NK modeling",
+    description="Utilities library for agent-based modeling using NK-framework",
+    long_description=longdesc,
+    long_description_content_type='text/markdown',
     url="https://github.com/ravshansk/nkpack",
     packages=setuptools.find_packages(),
     classifiers=[
