5c0ef5c744708e9f255c9b48e6988de54e58334e
ravshansk <rsk@ravshansk.com>
2020-12-19 05:57:49 +0100

    edited setup for new version and listed dependencies

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="nkpack", # Replace with your own username
-    version="1.0.0",
+    version="1.0.1",
     author="Ravshan S.K.",
     author_email="rsk@ravshansk.com",
     description="A library of utilities for NK modeling",
@@ -13,6 +13,8 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    install_requires=[
+        "numpy","scipy","itertools","numba"],
     python_requires='>=3.6',
 )
 
